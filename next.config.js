88ec08b5d21b4f365fb23c749889f97df713ba0e
Ramon Prata <ramon.prata@dtidigital.com.br>
2024-02-07 15:13:57 -0300

    mock api finished

--- next.config.js
+++ next.config.js
@@ -14,15 +14,6 @@ const nextConfig = {
           // value: process.env.ALLOWED_ORIGIN,
         },
         // Allows for specific methods accepted
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET, POST, PUT, DELETE, OPTIONS",
-        },
-        // Allows for specific headers accepted (These are a few standard ones)
-        {
-          key: "Access-Control-Allow-Headers",
-          value: "Content-Type, Authorization",
-        },
       ],
     },
   ],
